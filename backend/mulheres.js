753fc850998c28449996c3e21d9560cfd362f2f6
jessica-ssouza <jessicassouza.web@gmail.com>
2024-01-12 11:01:58 -0300

    subindo projeto completo Programaria

---
+++ backend/mulheres.js
@@ -0,0 +1,92 @@
+const express = require ("express") //iniciando o express
+const router = express.Router() //configurando a primeira parte da rota
+const cors = require('cors') // trazendo o pacote cors que permite consumir essa api no front-end
+const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo bancodeDados
+conectaBancoDeDados() //chamando a função que conecta ao banco de dados
+
+const Mulher = require('./mulherModel')
+
+const app = express() //iniciando o app
+app.use(express.json())
+app.use(cors())
+const porta = 3333 //criando a porta
+
+//get
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    }catch (erro) {
+        console.log(erro)
+    }
+}
+
+//post
+async function criaMulher(request, response) {                                                                                                                                                                                              
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+
+    try{
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada) //201 é o código para resposta criada com sucesso
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+                                           
+//patch
+async function corrigeMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
+        }
+
+        if (request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio
+        }
+
+        if (request.body.imagem) {
+            mulherEncontrada.imagem = request.body.imagem 
+        }
+
+        if (request.body.citacao) {
+            mulherEncontrada.citacao = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        
+        response.json(mulherAtualizadaNoBancoDeDados)
+    } catch (erro) {
+        console.log(erro)
+    }
+}
+
+//delete
+async function deletaMulher (request, response) {
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso!'})
+    } catch(erro) {
+            console.log(erro)
+    }
+}
+
+//rotas
+app.use(router.get('/mulheres', mostraMulheres)) //configurando a rota get/mulheres
+app.use(router.post('/mulheres', criaMulher)) //configurando a rota post/mulheres'
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //configurando a rota patch/mulheres
+app.use(router.delete('/mulheres/:id', deletaMulher)) //configurando a rota delete/mulheres
+
+//porta
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)    
+}
+
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
